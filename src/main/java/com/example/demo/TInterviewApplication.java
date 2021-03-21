a47a7026b665ea86f9c0fc53d1a3b6f55d38e78c
alam khorshed <mkalam7@gmail.com>
2021-03-21 09:38:13 -0400

    Baseline commit

---
+++ src/main/java/com/example/demo/TInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TInterviewApplication.class, args);
+	}
+
+}
